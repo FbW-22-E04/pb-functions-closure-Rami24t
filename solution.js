4d940b9da15cccc408564c2a583717534ccbc522
Rami <103028944+Rami24t@users.noreply.github.com>
2022-08-22 15:16:50 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,22 +1,18 @@
+// A closure is when a function is having access to its variables in the parent scope, even after the parent function has closed.
 //a. closure is used in the function inside the function personalDice because it is accessing name variable from the parent function.
-
 //b. Compare and contrast calling dansRoll the first and second time. What is always the same? What could change?
 // name still has the value 'Dan' and newRoll has now a new random number between 1 and 6
-
 //c.
 // The scope of the anonymous function 
-
-//3.
-
 // 
 
 
+//3.
 
 const createBase= function (base)
 {
   return function (num) {return base+num;}
 }
-
 
 const addSix = createBase(6);
 addSix(10); // returns 16
